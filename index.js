7875d3b13b4111eba64e31911be8ddf15a1272ff
Slimeball <alexramo2330@gmail.com>
2022-08-17 16:18:34 -0500

    prompt-switch-statements

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const dotenv = require('dotenv');
+const { init } = require('express/lib/application');
 dotenv.config();
 
 console.log("Welcome to Employee Tracker!!!");
@@ -14,3 +15,55 @@ const db = mysql.createConnection(
     },
     console.log(`Connected to the ${process.env.DB_NAME} database`)
 );
+
+dn.connect(err => {
+    if(err) throw err;
+    //starts inquirer prompts
+    begin();
+})
+
+function begin() {
+    inquirer.prompt(
+        {
+            type: 'list',
+            name: 'menu',
+            message: 'What would you like to do?',
+            choices: [
+                "View all employees",
+                "Add Employee",
+                "Update employee role",
+                "View roles",
+                "Add Role",
+                "View all departments",
+                "Add department",
+                "Quit"
+            ]
+        }).then(answer => {
+            switch (answer.menu) {
+                case "View all employees":
+                viewAllEmployees();
+                break;
+                case "Add Employee":
+                    addEmployee();
+                    break;
+                case "Update employee role":
+                    updateEmployeeRole();
+                    break;
+                case "View roles":
+                    viewAllRoles();
+                    break;
+                case "Add Role":
+                    addRole();
+                    break;
+                case "View all departments":
+                    viewAllDepartments();
+                    break;
+                case "Add department":
+                    addDepartment();
+                    break;
+                case "Quit":
+                    quit();
+                    break;
+            }
+        });
+}
